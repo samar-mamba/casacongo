0681108c23fe2bd7f9b99e1f8d61c23a02508f2e
samar mamba <mamsamar5@gamil.com>
2024-04-15 16:16:37 +0100

    init:initialisation de tailwind

---
+++ src/pages/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+
+    return (
+        <>
+        
+      <p  className="text-3xl font-bold underline">CasaCongo</p>
+        
+        </>
+    );
+    
+}
+
+export default Home
