7bbb614ea02c4cf8ad1388cc74e5ab0291c0a9e0
samar mamba <mamsamar5@gamil.com>
2024-04-16 11:28:07 +0100

    feat:add navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,10 +5,17 @@ function Header() {
 
     return (
         <>
-        <div className="flex flex-row justify-around items-center bg-[#1ACB6B] ">
+        <div className="flex flex-row justify-around items-center bg-green-300 p-5 ">
             <p className="text-orange-500">CongoCasa</p>
-            <button className="bg-orange-500 hover:bg-cyan-600">Deposer une annonce</button>
-            <button>Se connecter</button>
+            <button className="bg-orange-500 hover:bg-orange-700 rounded-md p-1 ">Deposer une annonce</button>
+            <button className="rounded-md">Se connecter</button>
+        </div>
+        <div className="">
+            <ul className="flex flex-row justify-around bg-orange-500 p-5 text-white ">
+                <li> <a href="#">Accueil</a></li>
+                <li><a href="#">Vente</a></li>
+                <li><a href="#">Location</a></li>
+            </ul>
         </div>
         
         </>
